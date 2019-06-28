d239b3a1c4744660601f643438a56b4087ec3c4c
rollmind <eyerama@gmail.com>
2019-06-28 21:44:28 +0900

    Initial Commit

---
+++ Lora/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Lora
+//
+//  Created by maylee on 2019/06/28.
+//  Copyright © 2019 rollmind. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
